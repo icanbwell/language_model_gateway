2cc6869f0835c420d938cb57017fb973b016866b
Imran Qureshi <imranq2@hotmail.com>
2025-02-01 12:40:40 -0800

    add well_known_configuration_reader to container

--- tests/gateway/test_protected.py
+++ tests/gateway/test_protected.py
@@ -1,4 +1,27 @@
+from typing import override, Optional
+
 import httpx
+
+from language_model_gateway.container.simple_container import SimpleContainer
+from language_model_gateway.gateway.api_container import get_container_async
+from language_model_gateway.gateway.utilities.tokens.well_known_configuration_reader.well_known_configuration import (
+    WellKnownConfiguration,
+)
+from language_model_gateway.gateway.utilities.tokens.well_known_configuration_reader.well_known_configuration_reader import (
+    WellKnownConfigurationReader,
+)
+
+
+class MockWellKnownConfigurationReader(WellKnownConfigurationReader):
+    @override
+    def read_from_well_known_configuration(
+        self, *, well_known_config_url: str
+    ) -> Optional[WellKnownConfiguration]:
+        return WellKnownConfiguration(
+            authorization_endpoint="https://example.com/auth",
+            token_endpoint="https://example.com/token",
+            userinfo_endpoint="https://example.com/userinfo",
+        )
 
 
 async def test_protected(async_client: httpx.AsyncClient) -> None:
@@ -14,6 +37,11 @@ async def test_protected(async_client: httpx.AsyncClient) -> None:
     assert redirect_location == "/auth/login"
 
     # call the /auth/callback endpoint
+    test_container: SimpleContainer = await get_container_async()
+    test_container.register(
+        WellKnownConfigurationReader, lambda c: MockWellKnownConfigurationReader()
+    )
+
     result = await async_client.get(redirect_location)
     print(result)
     assert result.status_code == 401
